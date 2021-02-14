891c2a76ffd3ea0a59a607f5a518d682d4e3dc0a
karim-ch <chaari.karim.plus@gmail.com>
2021-02-14 22:29:23 +0100

    changing default url

--- src/utils/client/githubSearcherClient.ts
+++ src/utils/client/githubSearcherClient.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 import { getEnv } from '../getEnv';
 
-const baseURL = getEnv('REACT_APP_CLIENT_URL', 'http://localhost:7000');
+const baseURL = getEnv(
+  'REACT_APP_CLIENT_URL',
+  'http://github-env.eba-5fd2gsz8.us-west-2.elasticbeanstalk.com',
+);
 
 const githubSearcherClient = axios.create({
   baseURL,
